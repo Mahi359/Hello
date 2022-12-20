5447538d4cf8118f731039eddad7be4c26fc3dcd
Mahi359 <71652278+Mahi359@users.noreply.github.com>
2022-12-20 09:02:29 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+let userInput;
+let movies=[];
+
+$(".add").click(function() {
+  userInput=$(".picture-url").val();
+    movies.push(userInput);
+    $(".gallery").append("<img src="+userInput+">");
+    $(".movies").val('');
+   
+});
